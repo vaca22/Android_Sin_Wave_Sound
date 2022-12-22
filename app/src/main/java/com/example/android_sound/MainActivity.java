c193dfe5126cf22b33c5608e232ba9e1c4d8326f
liguanghao <liguanghao@viatomtech.com>
2022-12-22 16:48:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android_sound/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.android_sound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private PlayThread mPlayThread;
+
+    Button btnPlay;
+    Button btnLeft;
+    Button btnRight;
+    Button btnStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnPlay = (Button) findViewById(R.id.btn_play);
+        btnLeft = (Button) findViewById(R.id.btn_left);
+        btnRight = (Button) findViewById(R.id.btn_right);
+        btnStop = (Button) findViewById(R.id.btn_stop);
+        btnPlay.setOnClickListener(this);
+        btnLeft.setOnClickListener(this);
+        btnRight.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            this.finish();
+            if (null != mPlayThread) {
+                mPlayThread.stopPlay();
+                mPlayThread = null;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_play:
+                playSound(true, true);
+                break;
+            case R.id.btn_left:
+                playSound(true, false);
+                break;
+            case R.id.btn_right:
+                playSound(false, true);
+                break;
+            case R.id.btn_stop:
+                if (null != mPlayThread) {
+                    mPlayThread.stopPlay();
+                    mPlayThread = null;
+                }
+                break;
+        }
+    }
+
+    private void playSound(boolean left, boolean right) {
+        if (null != mPlayThread) {
+            mPlayThread.stopPlay();
+            mPlayThread = null;
+        }
+        mPlayThread = new PlayThread(200);
+        mPlayThread.setChannel(left, right);
+        mPlayThread.start();
+    }
+}
---
+++ app/src/main/java/com/example/android_sound/PlayThread.java
@@ -0,0 +1,99 @@
+package com.example.android_sound;
+
+/**
+ * Created by Mr.Chan
+ * Time 2022-11-10
+ * Blog https://www.cnblogs.com/Frank-dev-blog/
+ */
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
+public class PlayThread extends Thread {
+    public static final int RATE = 44100;
+    AudioTrack mAudioTrack;
+    public static boolean ISPLAYSOUND;
+
+    /**
+     * 总长度
+     **/
+    int length;
+    /**
+     * 一个正弦波的长度
+     **/
+    int waveLen;
+    /**
+     * 频率
+     **/
+    int Hz;
+    /**
+     * 正弦波
+     **/
+    byte[] wave;
+
+    /**
+     * 初始化
+     * @param rate 频率
+     */
+    public PlayThread(int rate) {
+        if (rate > 0) {
+            Hz = rate;
+            waveLen = RATE / Hz;
+            length = waveLen * Hz;
+            wave = new byte[RATE];
+            mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, RATE,
+                    AudioFormat.CHANNEL_CONFIGURATION_STEREO, // CHANNEL_CONFIGURATION_MONO,
+                    AudioFormat.ENCODING_PCM_8BIT, length, AudioTrack.MODE_STREAM);
+            ISPLAYSOUND = true;
+            wave = SinWave.sin(wave, waveLen, length);
+        } else {
+            return;
+        }
+
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        if (null != mAudioTrack)
+            mAudioTrack.play();
+        //一直播放
+        while (ISPLAYSOUND) {
+            mAudioTrack.write(wave, 0, length);
+        }
+
+    }
+
+    /**
+     * 设置左右声道，左声道时设置右声道音量为0，右声道设置左声道音量为0
+     *
+     * @param left  左声道
+     * @param right 右声道
+     */
+    public void setChannel(boolean left, boolean right) {
+        if (null != mAudioTrack) {
+            mAudioTrack.setStereoVolume(left ? 1 : 0, right ? 1 : 0);
+        }
+    }
+
+    //设置音量
+    public void setVolume(float left, float right) {
+        if (null != mAudioTrack) {
+            mAudioTrack.setStereoVolume(left,right);
+        }
+    }
+
+    public void stopPlay() {
+        ISPLAYSOUND = false;
+        releaseAudioTrack();
+    }
+
+    private void releaseAudioTrack() {
+        if (null != mAudioTrack) {
+            mAudioTrack.stop();
+            mAudioTrack.release();
+            mAudioTrack = null;
+        }
+    }
+}
